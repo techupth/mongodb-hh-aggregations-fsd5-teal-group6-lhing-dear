e65926749b778b0e88a0d79e49ac22051827c279
Lhing <thorntan.r@gmail.com>
2024-01-11 16:23:10 +0700

    feat:completed ex1-7

--- exercise-6.js
+++ exercise-6.js
@@ -1,1 +1,11 @@
 // Paste the complete MQL here
+//ให้ Query หายอดขายทั้งหมดในเดือน 7 ของปี 2021
+db.pizzaOrders.aggregate([
+  {
+    $group: {
+      _id: { year: { $year: "$created_at" }, month: { $month: "$created_at" } },
+      total_sales: { $sum: "$total_price" },
+    },
+  },
+  { $match: { "_id.year": 2021, "_id.month": 7 } },
+]);
